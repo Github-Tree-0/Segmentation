8d33edbe3c4837c0e6f56e524c6421c71e7f050f
FreeButUselessSoul <576587568@qq.com>
2021-12-08 14:07:02 +0800

    cuda OOM, trying bigger

--- src/train.py
+++ src/train.py
@@ -16,7 +16,7 @@ EPOCHS = 2000
 writer = SummaryWriter('../log/')
 save_path = '../checkpoints/'
 path = '../data/complex/'
-train_data = URISC(path, mode="train", augmentation=True)
+train_data = URISC(path, mode="train",crop_size=572, augmentation=True,transform=torch.Tensor)
 val_data = URISC(path, mode='val')
 model = U_Net.U_Net(output_ch=1)
 model = model.cuda(device=0)
@@ -28,11 +28,14 @@ for epoch in tqdm(range(EPOCHS)):
     cr_loss = 0
     for it in range(iters):
         inp, gt = train_data[it]
+        inp = inp.cuda(device=0);gt=gt.cuda(device=0)
         optimizer.zero_grad()
         # import ipdb;ipdb.set_trace()
         # TODO: Adapt input to U-net.
+        print(inp.shape,gt.shape)
         pred = model(inp)
-        loss = criterion(pred, gt)
+        gt_cropped=transforms.CenterCrop(pred.detach().shape)(gt)
+        loss = criterion(pred, gt_cropped)
         loss.backward()
         optimizer.step()
         cr_loss += loss.detach()
